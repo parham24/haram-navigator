11988ab7302cac067601187e972326050028b8ce
parham24 <54466283+parham24@users.noreply.github.com>
2025-09-28 00:09:07 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // حتما این نسخه را در هر آپدیت تغییر دهید
-const CACHE_NAME = 'haram-map-v28'; 
+const CACHE_NAME = 'haram-map-v29'; 
 
 const urlsToCache = [
   './',
@@ -59,6 +59,7 @@ self.addEventListener('fetch', event => {
       })
   );
 });
+
 
 
 
